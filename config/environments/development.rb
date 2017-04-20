98ec0c2d35300905618a69e93ce3c562b94e777f
Sebastian <sebastiankurpios@gmail.com>
2017-04-20 12:20:54 +0200

    Host changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Devise mailer conf
-  config.action_mailer.default_url_options = { host: 'smtp.gmail.com' }
+  config.action_mailer.default_url_options = { host: 'todomanagerbtp.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
       {
