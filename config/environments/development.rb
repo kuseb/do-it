5275560617e49c276da01bb8e565f55bf0d8682b
Sebastian <sebastiankurpios@gmail.com>
2017-04-20 12:05:29 +0200

    Mail repaired

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,9 +46,18 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Devise mailer conf
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'smtp.gmail.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  ActionMailer::Base.smtp_settings =
+      {
+
+          :address            => 'smtp.gmail.com',
+          :port               => 587,
+          :domain             => 'gmail.com',
+          :authentication     => :plain,
+          :user_name          => 'doitappbst@gmail.com',
+          :password           => 'haslo123'
+      }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
